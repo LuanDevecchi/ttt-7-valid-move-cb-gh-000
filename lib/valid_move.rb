7348ef4ca11c05d269fb41e4b827f54333e4efb6
Luan Devecchi <gitluan@engineer.com>
2018-08-27 23:27:53 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,7 +8,7 @@ def position_taken?(board,index)
 end
 
 def valid_move?(board,index)
-  if !position_taken?(board,index) && index.between(0,8)
+  if index.between(0,8)  &&  !position_taken?(board,index)
     true
   else
     false
